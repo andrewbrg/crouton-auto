18d3c09bc0848655e0b282f2eed7416c4de07ce1
andrewbrg <andrewbrg87@gmail.com>
2017-10-07 21:38:17 +0200

    Update getUbuntu.sh

--- getUbuntu.sh
+++ getUbuntu.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
 
+# Prepare vars
+selfName=`basename $0`
+selfPath=`dirname $0`/$selfName
+croutonPath=`dirname $0`/crouton
+phpstormPath=/usr/local/bin/phpstorm
+robomongoPath=/usr/local/bin/robomongo
+bootstrapPath=`dirname $0`/xenial.tar.bz2
+downloadsPath=/home/chronos/user/Downloads
+chrootPath=/mnt/stateful_partition/crouton/chroots/xenial
+targets=cli-extra,xorg,xiwi,extension,keyboard,audio,chrome,gnome
+    
+    
 ###############################################################
-## Title helper
+## Helpers
 ###############################################################
 title() {
-    printf "\\n\\n"
-    printf "\\033[1;42m"
-    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' ' '
+    printf "\033[1;42m"
+    printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' ' '
     printf '%-*s\n' "${COLUMNS:-$(tput cols)}" "  # $1" | tr ' ' ' '
-    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' ' '
-    printf "\\033[0m"
-    printf "\\n"
+    printf '%*s'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' ' '
+    printf "\033[0m"
+    printf "\n\n"
     sleep .5
+}
+
+breakLine() {
+    printf "\n"
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    printf "\n\n"
+}
+
+askUser() {
+    while true; do
+        read -p " - $1 (y/n): " yn
+        case $yn in
+            [Yy]* ) echo 1; return 1;;
+            [Nn]* ) echo 0; return 0;;
+        esac
+    done
 }
 
 ###############################################################
 ## Installation
 ###############################################################
 install() {
-    # Prepare vars
-    selfName=`basename $0`
-    selfPath=`dirname $0`/$selfName
-    croutonPath=`dirname $0`/crouton
-    bootstrapPath=`dirname $0`/xenial.tar.bz2
-    downloadsPath=/home/chronos/user/Downloads
-    chrootPath=/mnt/stateful_partition/crouton/chroots/xenial
-    targets=cli-extra,xorg,xiwi,extension,keyboard,audio,chrome,gnome
-
     # Move to the downloads folder, we'll work in here
     cd $downloadsPath
 
@@ -34,6 +52,7 @@ install() {
     if [ ! -f $croutonPath ]; then
         title "Fetching latest crouton"
         wget https://goo.gl/fd3zc -O crouton
+        breakLine
     fi
     
     # If no chroot is setup
@@ -42,11 +61,15 @@ install() {
         if [ ! -f $bootstrapPath ]; then
             title "Preparing an Ubuntu bootstrap"
             sudo sh $croutonPath -d -f $bootstrapPath -r xenial -t $targets
+            breakLine
         fi
         
         # Setup Ubuntu
         title "Installing Ubuntu 16.04LTS in chroot"
-        sudo sh $croutonPath -f $bootstrapPath -t $targets
+        if [ "$(askUser "Install Ubuntu 16.04LTS")" -eq 1 ]; then
+            sudo sh $croutonPath -f $bootstrapPath -t $targets
+        fi
+        breakLine
     fi
     
     # Launch Ubuntu & configure
@@ -57,112 +80,204 @@ install() {
     sudo cp $selfPath $chrootPath/home/$chrootUsername/Downloads/$selfName
     sudo chmod +x $chrootPath/home/$chrootUsername/Downloads/$selfName
     sudo enter-chroot -n xenial -l sh /home/$chrootUsername/Downloads/$selfName
+    breakLine
 }
 
 ###############################################################
 ## Configuration
 ###############################################################
-configure() {
-    title "Installing prerequisites..."
+cPrerequisites() {
+    title "Installing prerequisites"
     sudo apt install -y locales software-properties-common python-software-properties
-    
-    title "Setting up repos..."
-    sudo add-apt-repository -y ppa:numix/ppa  
-    sudo add-apt-repository -y ppa:gnome3-team/gnome3-staging  
-    sudo add-apt-repository -y ppa:gnome3-team/gnome3  
-    sudo add-apt-repository -y ppa:notepadqq-team/notepadqq  
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6  
-    sudo echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list  
-    sudo apt install -y curl  
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg  
-    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg  
+    breakLine
+}
+
+cRepos() {
+    title "Setting up repos"
+    sudo add-apt-repository -y ppa:numix/ppa
+    sudo add-apt-repository -y ppa:gnome3-team/gnome3-staging
+    sudo add-apt-repository -y ppa:gnome3-team/gnome3
+    sudo add-apt-repository -y ppa:notepadqq-team/notepadqq
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+    sudo echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+    sudo apt install -y curl
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-    
-    title "Installing important tools..."
-    sudo apt update -y  
+    sudo apt update -y
+    breakLine
+}
+
+cUi() {
+    title "Preparing the Gnome UI"
+    sudo apt dist-upgrade -y
+    sudo apt install -y numix-icon-theme-circle gnome-tweak-tool gnome-terminal whoopsie gnome-control-center gnome-online-accounts
     sudo apt install -y language-pack-en-base nano mlocate htop notepadqq preload inxi
-    
-    title "Preparing the UI..."
-    sudo apt dist-upgrade -y  
-    sudo apt install -y numix-icon-theme-circle gnome-tweak-tool gnome-terminal whoopsie gnome-control-center gnome-online-accounts   
-    sudo apt install -y gnome-shell-extension-dashtodock gnome-software gnome-software-common gnome-shell-pomodoro  
-    
-    title "Installing PHP7.0..."
-    sudo apt install -y php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip  
-    
-    title "Installing Composer..."
-    sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer  
+    sudo apt install -y gnome-shell-extension-dashtodock gnome-software gnome-software-common gnome-shell-pomodoro
+    breakLine
+}
 
-    title "Installing NodeJS..."
-    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -  
-    sudo apt install -y build-essential nodejs  
-    
-    title "Installing Bower..."
-    sudo npm install -y bower -g  
-
-    title "Installing Gulp..."
-    sudo npm install -y gulp -g  
-
-    title "Installing MeteorJS..."
-    sudo curl https://install.meteor.com/ | sh  
-
-    title "Installing Git..."
-    sudo apt install -y git  
-
-    title "Installing MongoDB..."
-    sudo apt install -y mongodb-org
-    
-    title "Installing MySQL Server & Workbench..."
-    sudo apt install -y mysql-server  
-    sudo apt install -y mysql-workbench  
-    sudo mysql_secure_installation   
-    
-    title "Installing Robomongo..."
-    sudo apt install -y xcb  
-    wget https://download.robomongo.org/1.1.1/linux/robo3t-1.1.1-linux-x86_64-c93c6b0.tar.gz -O robomongo.tar.gz  
-    sudo mkdir /usr/local/bin/robomongo  
-    sudo tar xf robomongo.tar.gz  
-    sudo rm robomongo.tar.gz  
-    sudo mv robo3t-*/* /usr/local/bin/robomongo  
-    sudo rm -rf robo3t-*/  
-    sudo rm /usr/local/bin/robomongo/lib/libstdc++*  
-    sudo chmod +x /usr/local/bin/robomongo/bin/robo3t 
-    
-    title "Installing VS Code IDE..."
-    sudo apt install -y code
-    
-    title "Installing PHP Storm IDE..."
-    wget https://download.jetbrains.com/webide/PhpStorm-2017.2.4.tar.gz -O phpstorm.tar.gz  
-    sudo tar xf phpstorm.tar.gz  
-    sudo mkdir /usr/local/bin/phpstorm  
-    sudo mv PhpStorm-*/* /usr/local/bin/phpstorm  
-    sudo rm -rf PhpStorm-*/  
-    sudo rm phpstorm.tar.gz  
-    
-    title "Cleaning up..."
+cClean() {
+    title "Cleaning up"
     sudo echo "LANG=en_US.UTF-8" >> /etc/default/locale
     sudo echo "LANGUAGE=en_US.UTF-8" >> /etc/default/locale
     sudo echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
     sudo sed 's/.*XKBMODEL.*/XKBMODEL="chromebook"/' /etc/default/keyboard
     
     sudo apt remove -y xterm netsurf netsurf-common netsurf-fb netsurf-gtk
-    sudo apt --purge autoremove -y  
-    sudo updatedb 
-    
-    title "Finalise PHP Storm..."
-    cd /usr/local/bin/phpstorm/bin  
-    xiwi ./phpstorm.sh  
+    sudo apt --purge autoremove -y
+    sudo updatedb
+    breakLine
+}
 
-    title "Finalise Robomongo..."
-    cd /usr/local/bin/robomongo/bin  
-    xiwi ./robo3t 
+cPhp() {
+    title "PHP 7.0"
+    if [ "$(askUser "Install PHP7.0")" -eq 1 ]; then
+        sudo apt install -y php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip
+        
+        breakLine
+        title "Composer"
+        if [ "$(askUser "Install Composer")" -eq 1 ]; then
+            sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+        fi
+    fi
+    breakLine
+}
+
+cNodejs() {
+    title "Node JS"
+    if [ "$(askUser "Install NodeJS")" -eq 1 ]; then
+        curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+        sudo apt install -y build-essential nodejs
+        
+        breakLine
+        title "Bower"
+        if [ "$(askUser "Install Bower")" -eq 1 ]; then
+            sudo npm install -y bower -g
+        fi
+        
+        breakLine
+        title "Gulp"
+        if [ "$(askUser "Install Gulp")" -eq 1 ]; then
+            sudo npm install -y gulp -g
+        fi
+        
+        breakLine
+        title "Meteor JS"
+        if [ "$(askUser "Install MeteorJS")" -eq 1 ]; then
+            sudo curl https://install.meteor.com/ | sh
+        fi
+    fi
+    breakLine
+}
+
+cNginx() {
+    title "Nginx"
+    if [ "$(askUser "Install Nginx")" -eq 1 ]; then
+        sudo apt install -y nginx
+    fi
+    breakLine
+}
+
+cGit() {
+    title "Git"
+    if [ "$(askUser "Install Git")" -eq 1 ]; then
+        sudo apt install -y git
+    fi
+    breakLine
+}
+
+cMysql() {
+    title "MySQL Server"
+    if [ "$(askUser "Install MySQL Server")" -eq 1 ]; then
+        sudo apt install -y mysql-server
+   
+        breakLine
+        title "MySQL Workbench"
+        if [ "$(askUser "Install MySQL Workbench")" -eq 1 ]; then
+            sudo apt install -y mysql-workbench
+        fi
+    fi
+    breakLine
+}
+
+cMongodb() {
+    title "MongoDB"
+    if [ "$(askUser "Install MongoDB server")" -eq 1 ]; then
+        sudo apt install -y mongodb-org
+        
+        breakLine
+        title "RoboMongo (Robo 3T)"
+        if [ "$(askUser "Install RoboMongo")" -eq 1 ]; then
+            sudo apt install -y xcb
+            cd /tmp
+            wget https://download.robomongo.org/1.1.1/linux/robo3t-1.1.1-linux-x86_64-c93c6b0.tar.gz -O robomongo.tar.gz
+            
+            if [ -d $robomongoPath ]; then
+                sudo rm -rf $robomongoPath
+            fi
+            
+            sudo mkdir $robomongoPath
+            sudo tar xf robomongo.tar.gz
+            sudo rm robomongo.tar.gz
+            sudo mv robo3t-*/* $robomongoPath
+            sudo rm -rf robo3t-*/
+            sudo rm $robomongoPath/lib/libstdc++*
+            sudo chmod +x $robomongoPath/bin/robo3t
+        fi
+  fi
+  breakLine
+}
+
+cVscodeide() {
+    title "Microsoft VS Code IDE"
+    if [ "$(askUser "Install Microsoft VS Code IDE")" -eq 1 ]; then
+        sudo apt install -y code
+    fi
+    breakLine
+}
+
+cPhpstorm() {
+    title "PHP Storm IDE"
+    if [ "$(askUser "Install PHP Storm IDE")" -eq 1 ]; then
+        cd /tmp
+        wget https://download.jetbrains.com/webide/PhpStorm-2017.2.4.tar.gz -O phpstorm.tar.gz
+        sudo tar xf phpstorm.tar.gz
+        
+        if [ -d $phpstormPath ]; then
+            sudo rm -rf $phpstormPath
+        fi
+        
+        sudo mkdir $phpstormPath
+        sudo mv PhpStorm-*/* $phpstormPath
+        sudo rm -rf PhpStorm-*/
+        sudo rm phpstorm.tar.gz
+    fi
+    breakLine
+}
+
+configure() {
+    
+    cPrerequisites;
+    cRepos;
+    cUi;
+    
+    cPhp;
+    cNodejs;
+    cNginx;
+    cGit;
+    cMysql;
+    cMongodb;
+    cVscodeide;
+    cPhpstorm;
+    
+    cClean;
 }
 
 inodeNum=`ls -id / | awk '{print $1}'`
-if [ $inodeNum -eq 2 ]; 
-    then 
+if [ $inodeNum -eq 2 ];
+    then
         install
-    else 
+    else
         configure
-        sudo startgnome   
 fi
